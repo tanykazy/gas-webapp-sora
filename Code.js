9b9d357f175301bdf80fecc2a27225624e3f8f7e
tanykazy <tanykazy@gmail.com>
2022-02-23 18:00:04 +0900

    Use user spreadsheet

--- Code.js
+++ Code.js
@@ -6,10 +6,6 @@ function doGet(e) {
   return output;
 }
 
-function doPost(e) {
-  return doGet(e);
-}
-
 function getData(grade) {
   var cache = CacheService.getUserCache();
   var values = cache.get(grade);
@@ -33,16 +29,89 @@ function getData(grade) {
   return null;
 }
 
-function getProperty(key) {
-  var userProperties = PropertiesService.getUserProperties();
-  var value = userProperties.getProperty(key);
-  if (value !== null) {
-    return JSON.parse(value);
+function createUserFlashcard() {
+  newUserFlashcardFile_();
+}
+
+function existUserFlashcard() {
+  const file = getUserFlashcardFile_();
+  if (file !== null) {
+    return true;
+  }
+  return false;
+}
+
+function getFlashcardList() {
+  const file = getUserFlashcardFile_();
+  const spreadsheet = SpreadsheetApp.open(file);
+  const sheets = spreadsheet.getSheets();
+  let list = [];
+  for (const sheet of sheets) {
+    list.push(sheet.getName());
+  }
+  return list;
+}
+
+function fetchFlashcardData(sheetName) {
+  const cache = CacheService.getUserCache();
+  let values = cache.get(sheetName);
+  if (values !== null) {
+    return JSON.parse(values);
+  }
+  const file = getUserFlashcardFile_();
+  const spreadsheet = SpreadsheetApp.open(file);
+  const sheet = spreadsheet.getSheetByName(sheetName);
+  if (sheet === null) {
+    throw 'there is no sheet with the given name.';
+  }
+  const range = sheet.getDataRange();
+  values = range.getValues();
+  values.shift();
+  cache.put(sheetName, JSON.stringify(values));
+  return values;
+}
+
+function getFileById_(id) {
+  try {
+    return DriveApp.getFileById(id);
+  } catch (error) {
+    Logger.log(error);
   }
   return null;
 }
 
-function setProperty(key, value) {
+function getOriginFlashcardFile_() {
+  return getFileById_('13Y87ZXg57DuuYDRs-9VUzMA3rRKVZAgH5JjJJd5QGYQ');
+}
+
+function newUserFlashcardFile_() {
+  const file = getOriginFlashcardFile_().makeCopy();
+  setProperty_('id', file.getId());
+  return file;
+}
+
+function getUserFlashcardFile_() {
+  const id = getProperty_('id');
+  if (id !== null) {
+    const file = getFileById_(id);
+    if (file === null) {
+      return null;
+    }
+    if (file.isTrashed()) {
+      return null;
+    }
+    return file;
+  }
+  return null;
+}
+
+function getProperty_(key) {
+  var userProperties = PropertiesService.getUserProperties();
+  var value = userProperties.getProperty(key);
+  return JSON.parse(value);
+}
+
+function setProperty_(key, value) {
   var userProperties = PropertiesService.getUserProperties();
   userProperties.setProperty(key, JSON.stringify(value));
 }
